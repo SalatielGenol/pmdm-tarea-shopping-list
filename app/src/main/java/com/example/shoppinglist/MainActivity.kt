c59be9e539fd41cd775bc33581b2cbb206e846ed
SalatielGenol <salatiel@genol.es>
2023-03-12 14:25:14 +0100

    Modificado identificador de instancia de VM

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -13,14 +13,14 @@ import com.example.shoppinglist.ui.theme.ShoppingListTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val shoppingVM: ShoppingListViewModel by viewModels()
+        val viewModel: ShoppingListViewModel by viewModels()
         setContent {
             ShoppingListTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    ShoppingListScreen(shoppingVM)
+                    ShoppingListScreen(viewModel)
                 }
             }
         }
