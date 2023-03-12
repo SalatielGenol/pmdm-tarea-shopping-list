3c82adbe0d7349e038b52dd940e7dba753c1bb91
SalatielGenol <salatiel@genol.es>
2023-03-12 17:08:10 +0100

    Avanzando con la implementacion de añadir elementos

--- app/src/main/java/com/example/shoppinglist/ShoppingListScreen.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingListScreen.kt
@@ -2,15 +2,18 @@ package com.example.shoppinglist
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -20,45 +23,102 @@ fun ShoppingListScreen() {
     val viewModel: ShoppingListViewModel = viewModel()
     Scaffold(
         topBar = { TopAppBar(title = { Text(text = "Shopping List") }) },
-        floatingActionButton = { if (viewModel.checkedFlag)
-            FloatingManyClose { viewModel.closeManyItems() }
-        } ,
+        floatingActionButton = {
+            if (viewModel.checkedFlag)
+                FloatingManyClose { viewModel.closeManyItems() }
+        },
         floatingActionButtonPosition = FabPosition.Center
     ) { paddingValues ->
-        LazyColumn(
-            contentPadding = paddingValues,
-        ) {
-            items(
-                items = viewModel.items,
-                key = { item -> item.id }
-            ) { item ->
-                ShoppingListItem(
-                    modifier = Modifier
-                        .padding(
-                            horizontal = 10.dp,
-                            vertical = 10.dp
+        Column(Modifier.padding(paddingValues)) {
+            AddButtonConvertible(viewModel)
+            /*BasicTextField(
+                value = viewModel.inputItemText,
+                onValueChange = { viewModel.inputItemTextValueChante(it) },
+                singleLine = true,
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        viewModel.newItem(
+                            viewModel.inputItemText
                         )
-                        .background(
-                            MaterialTheme.colors.secondary,
-                            shape = RoundedCornerShape(15.dp)
-                        )
-                        .animateItemPlacement(),
-                    itemName = item.name,
-                    checkValue = item.checkValue,
-                    onCheckedChange = { viewModel.onChangeCheck(item) },
-                    onItemClose = { viewModel.close(item) },
+                    },
                 )
+            )*/
+            LazyColumn {
+                items(
+                    items = viewModel.items,
+                    key = { item -> item.id }
+                ) { item ->
+                    ShoppingListItem(
+                        modifier = Modifier
+                            .padding(
+                                horizontal = 10.dp,
+                                vertical = 10.dp
+                            )
+                            .background(
+                                MaterialTheme.colors.secondary,
+                                shape = RoundedCornerShape(15.dp)
+                            )
+                            .animateItemPlacement(),
+                        itemName = item.name,
+                        checkValue = item.checkValue,
+                        onCheckedChange = { viewModel.onChangeCheck(item) },
+                        onItemClose = { viewModel.close(item) },
+                    )
+                }
             }
         }
     }
 }
 
 @Composable
-fun FloatingManyClose(onClick: () -> Unit){
+fun FloatingManyClose(onClick: () -> Unit) {
     FloatingActionButton(
         onClick = onClick,
         backgroundColor = MaterialTheme.colors.primary
     ) {
         Icon(Icons.Filled.Close, contentDescription = "Close")
+    }
+}
+
+@Composable
+fun AddButtonConvertible(viewModel: ShoppingListViewModel) {
+    Box(
+        modifier = Modifier
+            .padding(
+                horizontal = 35.dp,
+                vertical = 5.dp
+            ), contentAlignment = Alignment.Center
+    ) {
+
+        TextField(
+            value = viewModel.inputItemText,
+            onValueChange = { viewModel.inputItemTextValueChante(it) },
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = { Text(text = "Something") },
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    viewModel.newItem(
+                        viewModel.inputItemText
+                    )
+                },
+            ),
+            singleLine = true,
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+        )
+        var visible by remember { mutableStateOf(true) }
+
+        if (visible) {
+            Button(
+                onClick = { visible = false }, modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Text(text = "Añadir elemento", modifier = Modifier.padding(5.dp))
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/shoppinglist/ShoppingListViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingListViewModel.kt
@@ -6,26 +6,46 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
 
-class ShoppingListViewModel: ViewModel(){
+class ShoppingListViewModel : ViewModel() {
     private val _items = getShoppingListItems().toMutableStateList()
-    var checkedFlag by mutableStateOf(false)
     val items: List<ShoppingDataItem>
-        get() = _items
+        get() = _items.asReversed()
 
-    fun close(item: ShoppingDataItem){
+    var checkedFlag by mutableStateOf(false)
+        private set
+
+    var inputItemText: String by mutableStateOf("")
+        private set
+
+    fun close(item: ShoppingDataItem) {
         _items.remove(element = item)
     }
 
-    fun onChangeCheck(item: ShoppingDataItem){
+    fun onChangeCheck(item: ShoppingDataItem) {
         item.checkValue = !item.checkValue
         checkedFlag = items.any { ShoppingDataItem -> ShoppingDataItem.checkValue }
     }
 
-    fun closeManyItems(){
+    fun closeManyItems() {
         _items.removeIf { ShoppingDataItem -> ShoppingDataItem.checkValue }
         checkedFlag = items.any { ShoppingDataItem -> ShoppingDataItem.checkValue }
+    }
+
+    fun inputItemTextValueChante(text: String) {
+        inputItemText = text
+    }
+
+    fun newItem(itemName: String) {
+        _items.add(
+            element = ShoppingDataItem(
+                id = _items.size + 1,
+                name = itemName,
+                checked = false
+            )
+        )
     }
 }
 
 
-private fun getShoppingListItems() = List(30) { i -> ShoppingDataItem(id = i, name = "Producto $i", checked = false) }
+private fun getShoppingListItems() =
+    List(30) { i -> ShoppingDataItem(id = i, name = "Producto $i", checked = false) }
