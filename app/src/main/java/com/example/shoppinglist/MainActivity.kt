50cef1a7f973c168bd1770bdc2192130e2c0538b
SalatielGenol <salatiel@genol.es>
2023-04-18 23:09:18 +0200

    Añadida linea a la activity para que no se esconda el teclado al cambiar el tema

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.shoppinglist
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +12,7 @@ import com.example.shoppinglist.ui.theme.ShoppingListTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_UNCHANGED)
         super.onCreate(savedInstanceState)
         setContent {
             ShoppingListTheme {
